57709bee7237780e82d5cda1b7842c903a29cdd2
podhmo <ababjam61+github@gmail.com>
2018-05-23 21:39:51 +0900

    reify

--- miniconfig/__init__.py
+++ miniconfig/__init__.py
@@ -115,3 +115,22 @@ class ConfiguratorCore(object):
         if callable(attr):
             return partial(attr, self)
         return attr
+
+
+# stolen from pyramid
+class reify(object):
+    """cached property"""
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        try:
+            self.__doc__ = wrapped.__doc__
+        except:
+            pass
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
