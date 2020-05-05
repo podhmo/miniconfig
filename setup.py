dca1732f29b1842e954de59a1bfb083a4d0662e9
podhmo <ababjam61+github@gmail.com>
2020-05-05 21:45:37 +0900

    Maintenance3 (#14)
    * chore: add .coveragerc
    
    * chore: pytest-cov
    
    * chore: add flake8
    
    * chore: oops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 install_requires = []
 dev_requires = ["black", "flake8", "mypy"]
-tests_requires = ["pytest"]
+tests_requires = ["pytest", "pytest-cov"]
 
 setup(
     classifiers=[
